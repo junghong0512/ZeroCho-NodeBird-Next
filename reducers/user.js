31426883e7395cd562fa5f1b0d84948397d78e83
Jung Hong <junghong@ninewatt.com>
2022-06-19 15:49:12 +0900

    How to use redux-thunk - 220619

--- reducers/user.js
+++ reducers/user.js
@@ -5,15 +5,47 @@ export const initialState = {
   loginData: {},
 };
 
-// ACTION CREATOR(로그인)
-export const loginAction = (data) => ({
-  type: 'LOG_IN',
+export const loginAction = (data) => {
+  return (dispatch) => {
+    dispatch(loginRequestAction());
+    axios
+      .post('/api/login')
+      .then((res) => {
+        dispatch(loginSuccessAction(res.data));
+      })
+      .catch((err) => {
+        dispatch(loginRequestFailure(err));
+      });
+  };
+};
+
+// ACTION CREATOR(로그인 요청)
+export const loginRequestAction = (data) => ({
+  type: 'LOG_IN_REQUEST',
+  data,
+});
+// ACTION CREATOR(로그인 성공)
+export const loginSuccessAction = (data) => ({
+  type: 'LOG_IN_SUCCESS',
+  data,
+});
+// ACTION CREATOR(로그인 실패)
+export const loginFailureAction = (data) => ({
+  type: 'LOG_IN_FAILURE',
   data,
 });
 
-// ACTION CREATOR(로그아웃)
-export const logoutAction = () => ({
-  type: 'LOG_OUT',
+// ACTION CREATOR(로그아웃 요청)
+export const logoutRequestAction = () => ({
+  type: 'LOG_OUT_REQUEST',
+});
+// ACTION CREATOR(로그아웃 성공)
+export const logoutSuccessAction = () => ({
+  type: 'LOG_OUT_SUCCESS',
+});
+// ACTION CREATOR(로그아웃 실패)
+export const logoutFailureAction = () => ({
+  type: 'LOG_OUT_FAILURE',
 });
 
 const reducer = (state = initialState, action) => {
